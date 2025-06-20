a35a608a67073de4baf50f9254ab5805db1592fd
mashiur <mashiurrahman1285@gmail.com>
2025-06-21 01:49:48 +0600

    Initial commit: Laravel 11 + Inertia + Vue + PostgreSQL setup

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="flex justify-between">
+        <p class="bg-green-600">This is the welcome page of our application.</p>
+        <p>Feel free to explore the features and functionalities.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Welcome',
+};
+</script>
+
+<style scoped>
+</style>
