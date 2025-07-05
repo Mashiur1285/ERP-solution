0cb86aef4d4047cf522905154c0dd4098a374dd4
rezuan98 <rezuanahmmeds@gmail.com>
2025-07-06 01:08:44 +0600

    completed category backend

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -6,10 +6,14 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Contracts\SupplierContract;
 use App\Contracts\DepositContract;
+use App\Contracts\CategoryContract;
 use App\Contracts\ProductPurchaseContract;
 use App\Repositories\ProductPurchaseRepository;
 use App\Repositories\SupplierRepository;
 use App\Repositories\DepositRepository;
+use App\Repositories\CategoryRepository;
+
+
 
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -19,5 +23,6 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(SupplierContract::class, SupplierRepository::class);
         $this->app->bind(DepositContract::class, DepositRepository::class);
         $this->app->bind(ProductPurchaseContract::class, ProductPurchaseRepository::class);
+        $this->app->bind(CategoryContract::class,CategoryRepository::class);
     }
 }
